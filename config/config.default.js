08d51a8e90a174f6d008c5b2e2b483712d59b1f1
DanielWLam <lwd931227@126.com>
2017-04-26 16:29:20 +0800

    initialize frontend and backend

---
+++ config/config.default.js
@@ -0,0 +1,10 @@
+'use strict';
+
+module.exports = appInfo => {
+  const config = {};
+
+  // should change to your own
+  config.keys = appInfo.name + '_1493193926791_3420';
+
+  return config;
+};
